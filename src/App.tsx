0c023302a18556c0f35d1b44886a38e26c84f889
Karan Janthe <karanjanthe@gmail.com>
2023-10-14 23:59:23 +0530

    basic demo is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import  { run } from 'wasm-pdf'
+import pdfDataJson from "../template.json"
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+  const createPdf = () => {
+    // @ts-ignore
+    window.pdfFileBlobURL = null;
+    // @ts-ignore
+    window.generatePDF = (data:any) => {
+      const blob = new Blob([data], {
+        type: 'application/pdf'
+      });
+      // @ts-ignore
+      if (window.pdfFileBlobURL !== null) {
+        // @ts-ignore
+        URL.revokeObjectURL(window.pdfFileBlobURL);
+      }
+      // @ts-ignore
+      window.pdfFileBlobURL = URL.createObjectURL(blob);
+      // for debugging purposes, open another window
+      //window.open(pdfFileBlobURL, "_blank");
+      // @ts-ignore
+      window.location.href = window.pdfFileBlobURL;
+    }
+    console.log(window)
+    const pdfData = run(pdfDataJson)
+    
+
+    
+    console.log(pdfData)
+  } 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Hello Karan</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={createPdf}>
+          Create Pdf
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
